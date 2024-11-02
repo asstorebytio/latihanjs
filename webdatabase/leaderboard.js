9a0b157487188ec33bb33f99c4fc23bce6767c4b
zpyoo <150746635+asstorebytio@users.noreply.github.com>
2024-11-02 19:24:07 +0800

    Update leaderboard.js

--- webdatabase/leaderboard.js
+++ webdatabase/leaderboard.js
@@ -1,5 +1,5 @@
 function fetchLeaderboard() {
-    fetch('https://asstorebytio.github.io/latihanjs/webdatabase/leaderboard.html')
+    fetch('https://latihanjs-tios-projects-a47fc15a.vercel.app/webdatabase/leaderboard')
         .then(response => response.json())
         .then(data => {
             const leaderboardList = document.getElementById("leaderboardList");
